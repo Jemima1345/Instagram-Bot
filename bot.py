059f98191c654bff9b71642df21115dda8ad7d2c
jem <jemima1345@github.com>
2020-08-09 21:46:51 -0600

    added the log in method

--- bot.py
+++ bot.py
@@ -12,6 +12,23 @@ class InstagramBot:
         self.password = password
         
         self.driver.get('https://www.instagram.com/')
+        time.sleep(2)
+        
+    
+    def log_in(self):
+        """
+        The bot logs into instagram and clicks not now to prevent storing log in info 
+        and again to prevent turning on notifications
+        """
 
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]').send_keys(self.username)
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]').send_keys(self.password)
+        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(2)
 
+        
 my_bot = InstagramBot(username, password)
+my_bot.log_in()
