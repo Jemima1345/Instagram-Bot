a3ec5ddfd6f11689042aa4741451610d14847155
jem <jemima1345@github.com>
2020-08-10 01:20:27 -0600

    added the nav_user and open_users_followers methods. Also added replaced login_info.py with a config file

--- bot.py
+++ bot.py
@@ -1,17 +1,32 @@
 from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
 import time
 import os
-from login_info import username
-from login_info import password
+from configparser import ConfigParser
+#from login_info import username
+#from login_info import password
 
 
 class InstagramBot:
-    def __init__(self, username, password):       
-        self.driver = webdriver.Chrome('chromedriver')
-        self.username = username
-        self.password = password
+    def __init__(self):       
+        """
+        Creates an instance of the InstagramBot class  
+        """
+        #chrome_options = Options()
+        #chrome_options.add_argument("--headless")
+        #chrome_options.add_argument('--no-sandbox')
         
-        self.driver.get('https://www.instagram.com/')
+        config = ConfigParser()
+        config_file = 'config.ini'
+        config.read(config_file)    
+        self.username = config['IG_AUTH']['USERNAME']
+        self.password = config['IG_AUTH']['PASSWORD']
+        self.login_url = config['IG_URLS']['login_url']
+        self.user_url = config['IG_URLS']['user_url']
+        self.users_followers_url = config['IG_URLS']['users_followers_url']
+        
+        self.driver = webdriver.Chrome('chromedriver')
+        self.driver.get(self.login_url)
         time.sleep(2)
         
     
@@ -27,8 +42,27 @@ class InstagramBot:
         time.sleep(5)
         self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
         self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        time.sleep(1)
+       
+        
+    def nav_user(self, user):
+        """
+        Navigates to a specified user's page
+        """
+        self.driver.get(self.user_url.format(user))
+        
+        
+    def open_users_followers(self, user):
+        """
+        Navigates to a specified user's follower list
+        """
+        self.driver.get(self.users_followers_url.format(user))
         time.sleep(2)
 
         
-my_bot = InstagramBot(username, password)
+my_bot = InstagramBot()
 my_bot.log_in()
+
+account = "guitarcenter"
+#my_bot.nav_user(account)
+my_bot.open_users_followers(account)
