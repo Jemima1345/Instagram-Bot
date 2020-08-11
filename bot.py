5c877014a02f91400db5293893ede3f76f15a2a9
jem <jemima1345@github.com>
2020-08-11 00:44:49 -0600

    fixed the open_users_followers method, added the follow user method, and changed the xpath for the not now buttons

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 import time
 import os
 from configparser import ConfigParser
-#from login_info import username
-#from login_info import password
 
 
 class InstagramBot:
@@ -21,9 +19,10 @@ class InstagramBot:
         config.read(config_file)    
         self.username = config['IG_AUTH']['USERNAME']
         self.password = config['IG_AUTH']['PASSWORD']
+        
         self.login_url = config['IG_URLS']['login_url']
         self.user_url = config['IG_URLS']['user_url']
-        self.users_followers_url = config['IG_URLS']['users_followers_url']
+        #self.users_followers_url = config['IG_URLS']['users_followers_url']
         
         self.driver = webdriver.Chrome('chromedriver')
         self.driver.get(self.login_url)
@@ -35,28 +34,58 @@ class InstagramBot:
         The bot logs into instagram and clicks not now to prevent storing log in info 
         and again to prevent turning on notifications
         """
-
-        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]').send_keys(self.username)
-        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]').send_keys(self.password)
-        self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
-        time.sleep(5)
-        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
-        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+        
+        username_input = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]')
+        username_input.send_keys(self.username)
+        password_input = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input[@class="_2hvTZ pexuQ zyHYP"]')
+        password_input.send_keys(self.password)
+        login_btn = self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
+        login_btn.click()
+        time.sleep(3)
+        
+        not_now_btn1 = self.driver.find_element_by_xpath('//*[text() = "Not Now"]')
+        not_now_btn1.click()
+        #must redefine btn to avoid error
+        not_now_btn2 = self.driver.find_element_by_xpath('//*[text() = "Not Now"]')
+        not_now_btn2.click()
         time.sleep(1)
-       
+        
+        #The following also works
+        #not_now_btn1 = self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')
+        #not_now_btn1.click()
+        #not_now_btn2 = self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+        #not_now_btn2.click()
+        #time.sleep(1)
+        
         
     def nav_user(self, user):
         """
         Navigates to a specified user's page
         """
         self.driver.get(self.user_url.format(user))
+        time.sleep(1)
         
         
-    def open_users_followers(self, user):
+    def follow_user(self):
         """
-        Navigates to a specified user's follower list
+        Follows a user when on their page
         """
-        self.driver.get(self.users_followers_url.format(user))
+        #follow_btn = self.driver.find_element_by_xpath('//*[contains(@class,"_6VtSN")')
+        #follow_btn = self.driver.find_element_by_xpath("//button[contains(text(),'Follow')")
+        
+        #follow_btn = self.driver.find_element_by_xpath('//*[@class="_5f5mN       jIbKX  _6VtSN     yZn4P   "]') #also works
+        follow_btn = self.driver.find_element_by_xpath('//*[text() = "Follow"]')
+        follow_btn.click()
+        time.sleep(2)
+
+    
+    def open_users_followers(self): #, user):
+        """
+        Opens a user's follower list when the user's page is already open
+        """
+        #self.driver.get(self.users_followers_url.format(user))
+        followers_btn = self.driver.find_element_by_partial_link_text("followers")
+        followers_btn.click()
         time.sleep(2)
 
         
@@ -64,5 +93,11 @@ my_bot = InstagramBot()
 my_bot.log_in()
 
 account = "guitarcenter"
-#my_bot.nav_user(account)
-my_bot.open_users_followers(account)
+my_bot.nav_user(account)
+
+#TODO: if it finds the follow button, follow user
+#my_bot.follow_user()
+
+
+my_bot.open_users_followers()
+my_bot.follow_user()
